70e04d1782447280a107a8ed89a8ae2d96009f3b
David Maser <davidemaser@gmail.com>
2017-01-09 10:54:24 -0500

    Initial

---
+++ src/main.js
@@ -0,0 +1,18 @@
+/**
+ * Created by altitude on 09/01/17.
+ */
+var Core = {
+    Config: {
+        targets:['div','span','p'],
+        plugins: {
+            translate: true,
+            encode: false
+        }
+    },
+    Init:{
+        Matches:function(){
+
+        }
+    }
+};
+var $c = c = Core;
