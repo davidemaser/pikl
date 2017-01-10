50ac0fa750819b75ce4cd52b42731b763c967775
David Maser <davidemaser@gmail.com>
2017-01-10 13:34:43 -0500

    Add form object

--- src/pikl.js
+++ src/pikl.js
@@ -16,6 +16,9 @@ var Pikl = {
     Ajax:{
         params:['src','index','node','repeat']
     },
+    Form:{
+        closures:['select','button','form']
+    },
     Index:{},
     Init:{
         Json:function(){
@@ -28,7 +31,7 @@ var Pikl = {
                 method:'GET',
                 success:function(data){
                     $.each(data,function(key,value){
-                        Pikl.Index[key] = value;
+                        pi[key] = value;
                     })
                 },error:function(){
                 },complete:function(){
@@ -42,6 +45,18 @@ var Pikl = {
             })
         },
         Content:function(node){
+            function genFormTag(obj,content){
+                var formObject = [];
+                var formString = '';
+                for(var o in obj){
+                    formObject.push(obj[o].split('='));
+                }
+                for(var f in formObject){
+                    var root = formObject[f];
+                    formString += ' '+root[0]+'="'+root[1]+'"';
+                }
+                return '<form'+formString+'>'+content+'</form>';
+            }
            var appendComma = false,targetObject={},targetBinding='',targetNode,targetContent,conditional,conditionType,conditionArgument,conditionCase={},t;
             node = node || 'body';
             $('tpl').each(function(){
@@ -49,95 +64,124 @@ var Pikl = {
                 targetBinding = $(this).attr('is');
                 targetObject = pi[targetBinding];
                 targetContent = $(this).html();
-                if(targetContent.indexOf('@') > -1){
-                    //build condition
-                    conditional = targetContent.split('@')[1].split('}')[0];
-                    conditionType = conditional.split(' ')[0];
-                    conditionArgument = conditional.indexOf('if') > -1 ? conditional.split('if')[1].trim() : '';
-                    conditionCase.true = targetContent.replace(conditional,'').replace('{@}','').split('{')[0];
-                    if(targetContent.indexOf('{@else}')>-1){
-                        conditionCase.false = targetContent.replace(conditional,'').replace('{@}','').split('{')[1].replace('@else}','');
-                    }
-                    console.log(conditionType);
-                    switch(conditionType){
-                        case 'if':
-                            var dateUnit = Pikl.Assistants.Date();
-                            if(conditionArgument.indexOf('and')>-1){
-                                conditionArgument = conditionArgument.split('and');
+                if(targetBinding == 'form'){
+                    var formTagObject = $(this).text().replace('{@','').split('}')[0].split(',');
+                    var formObject = {};
+                    var itemCount = 1;
+                    $('tpl[is="form"]').find('tpl').each(function(){
+                        /*
+                        collect form items within template
+                         */
+                        var formItem = $(this).html().replace(/@/g,'').replace(/{/g,'').replace(/}/g,'').split(',');
+                        formObject[itemCount] = formItem;
+                        itemCount++;
+                    });
+                    var formString = ''
+                    for(var f in formObject){
+                        var obj = formObject[f];
+                        formString += '<';
+                        var thisFormObjectType = obj[0];
+                        for(var o in obj){
+                            var objFin = obj[o].split('=');
+                            if(objFin.length == 1){
+                                //single object like name or disabled
+                                formString += objFin[0]+' ';
+                            }else{
+                                formString += objFin[0]+'="'+objFin[1]+'" ';
                             }
-                            for(var c in conditionArgument){
-                                var calc = conditionArgument[c].trim();
-                                calc = calc.split(' ');
-                                for(var d in calc){
-                                    if($.inArray(calc[d],Pikl.DateConditions) > -1){
-                                        if(Pikl.Assistants.Comparison(dateUnit[calc[d]],calc[1],parseInt(calc[2]))){
-                                            $('<div>' + conditionCase.true + '</div>').insertBefore($(this));
-                                            $(this).remove();
-                                            console.log(Pikl.Assistants.Comparison(dateUnit[calc[d]],calc[1],parseInt(calc[2])))
-                                        }else{
-                                            $('<div>' + conditionCase.false + '</div>').insertBefore($(this));
-                                            $(this).remove();
-                                            console.log(Pikl.Assistants.Comparison(dateUnit[calc[d]],calc[1],parseInt(calc[2])))
+                        }
+                        formString += $.inArray(thisFormObjectType,Pikl.Form.closures) > 1 ? '</'+thisFormObjectType+'>': '/>';
+                    }
+                    $('<div>' + genFormTag(formTagObject,formString) + '</div>').insertBefore($(this));
+                    $(this).remove();
+                }else if($(this).parent().attr('is') !== 'form'){
+                    if (targetContent.indexOf('@') > -1) {
+                        //we have functions
+                        conditional = targetContent.split('@')[1].split('}')[0];
+                        conditionType = conditional.split(' ')[0].replace(/,/g, '');
+                        conditionArgument = conditional.indexOf('if') > -1 ? conditional.split('if')[1].trim() : '';
+                        conditionCase.true = targetContent.replace(conditional, '').replace('{@}', '').split('{')[0];
+                        if (targetContent.indexOf('{@else}') > -1) {
+                            conditionCase.false = targetContent.replace(conditional, '').replace('{@}', '').split('{')[1].replace('@else}', '');
+                        }
+                        console.log(conditionType);
+                        switch (conditionType) {
+                            case 'if':
+                                var dateUnit = Pikl.Assistants.Date();
+                                if (conditionArgument.indexOf('and') > -1) {
+                                    conditionArgument = conditionArgument.split('and');
+                                }
+                                for (var c in conditionArgument) {
+                                    var calc = conditionArgument[c].trim();
+                                    calc = calc.split(' ');
+                                    for (var d in calc) {
+                                        if ($.inArray(calc[d], Pikl.DateConditions) > -1) {
+                                            if (Pikl.Assistants.Comparison(dateUnit[calc[d]], calc[1], parseInt(calc[2]))) {
+                                                $('<div>' + conditionCase.true + '</div>').insertBefore($(this));
+                                                $(this).remove();
+                                            } else {
+                                                $('<div>' + conditionCase.false + '</div>').insertBefore($(this));
+                                                $(this).remove();
+                                            }
                                         }
                                     }
                                 }
-                            }
-                            break;
-                        case 'calc':
-                            var calcString = targetContent.replace('{@calc}','').replace('{/calc}','');
-                            var calcMethod = calcString.split('{@')[1].split('}')[0];
-                            var calcValues = calcString.split('{@'+calcMethod+'}');
-                            $('<div>' + Pikl.Assistants.Calculate(calcValues[0],calcMethod,calcValues[1]) + '</div>').insertBefore($(this));
-                            $(this).remove();
-                            break;
-                        case 'date':
-                            var dateString = targetContent.replace('{@date}','').replace('{/date}','');
-                            $('<div>' + Pikl.Assistants.Date(true) + '</div>').insertBefore($(this));
-                            $(this).remove();
-                            break;
-                        case 'ajax':
-                            var _this = this;
-                            var ajaxString = targetContent.replace('{@ajax}','').replace('{/ajax}','');
-                            var ajaxParams = ajaxString.split(',');
-                            var ajaxObject = {};
-                            var returnedData = '';
-                            for(var p in ajaxParams){
-                                var param = ajaxParams[p].split('=')[0].replace('}','').replace('{','').replace('@','');
-                                var value = ajaxParams[p].split('=')[1].replace('}','').replace('{','').replace('@','');
-                                if($.inArray(param,Pikl.Ajax.params)>-1){
-                                    ajaxObject[param] = value;
-                                }
-                            }
-                            Pikl.Assistants.Ajax(ajaxObject).done(function(result) {
-                                result = ajaxObject.index !== undefined && ajaxObject.index !== '' ? result[ajaxObject.index] : result;
-                                if (ajaxObject.repeat == true || ajaxObject.repeat === 'true'){
-                                    for (var r in result) {
-                                        returnedData += ajaxObject.node !== undefined && ajaxObject.node !== '' ? result[r][ajaxObject.node] !== undefined ? result[r][ajaxObject.node]+' ' : '' : result[r]+' ';
+                                break;
+                            case 'calc':
+                                var calcString = targetContent.replace('{@calc}', '').replace('{/calc}', '');
+                                var calcMethod = calcString.split('{@')[1].split('}')[0];
+                                var calcValues = calcString.split('{@' + calcMethod + '}');
+                                $('<div>' + Pikl.Assistants.Calculate(calcValues[0], calcMethod, calcValues[1]) + '</div>').insertBefore($(this));
+                                $(this).remove();
+                                break;
+                            case 'date':
+                                var dateString = targetContent.replace('{@date}', '').replace('{/date}', '');
+                                $('<div>' + Pikl.Assistants.Date(true) + '</div>').insertBefore($(this));
+                                $(this).remove();
+                                break;
+                            case 'ajax':
+                                var _this = this;
+                                var ajaxString = targetContent.replace('{@ajax}', '').replace('{/ajax}', '');
+                                var ajaxParams = ajaxString.split(',');
+                                var ajaxObject = {};
+                                var returnedData = '';
+                                for (var p in ajaxParams) {
+                                    var param = ajaxParams[p].split('=')[0].replace('}', '').replace('{', '').replace('@', '');
+                                    var value = ajaxParams[p].split('=')[1].replace('}', '').replace('{', '').replace('@', '');
+                                    if ($.inArray(param, Pikl.Ajax.params) > -1) {
+                                        ajaxObject[param] = value;
                                     }
                                 }
-                                $('<div>' + returnedData + '</div>').insertBefore($(_this));
-                                $(_this).remove();
-                                console.log(returnedData);
-                            }).fail(function() {
-                                console.log('could not load json data');
-                            });
-                            break;
-                    }
-                }else{
-                    targetNode = $(this).html().replace(/}/g,'').replace(/{/g,'').replace('#comma','');
-                    appendComma = $(this).html().indexOf('{#comma}') > -1;
-                    if(typeof targetObject == 'object'){
-                        for(t in targetObject){
-                            keyString += targetObject[t][targetNode] !== undefined ? targetObject[t][targetNode] : ' ';
-                            if(appendComma){
-                                keyString += ', ';
+                                Pikl.Assistants.Ajax(ajaxObject).done(function (result) {
+                                    result = ajaxObject.index !== undefined && ajaxObject.index !== '' ? result[ajaxObject.index] : result;
+                                    if (ajaxObject.repeat == true || ajaxObject.repeat === 'true') {
+                                        for (var r in result) {
+                                            returnedData += ajaxObject.node !== undefined && ajaxObject.node !== '' ? result[r][ajaxObject.node] !== undefined ? result[r][ajaxObject.node] + ' ' : '' : result[r] + ' ';
+                                        }
+                                    }
+                                    $('<div>' + returnedData + '</div>').insertBefore($(_this));
+                                    $(_this).remove();
+                                }).fail(function () {
+                                    console.log('could not load json data');
+                                });
+                                break;
+                        }
+                    } else {
+                        targetNode = $(this).html().replace(/}/g, '').replace(/{/g, '').replace('#comma', '');
+                        appendComma = $(this).html().indexOf('{#comma}') > -1;
+                        if (typeof targetObject == 'object') {
+                            for (t in targetObject) {
+                                keyString += targetObject[t][targetNode] !== undefined ? targetObject[t][targetNode] : ' ';
+                                if (appendComma) {
+                                    keyString += ', ';
+                                }
                             }
                         }
+                        keyString = keyString.trim();
+                        keyString = keyString.slice(-1) == ',' ? keyString.substring(0, keyString.length - 1) : keyString;
+                        $('<div>' + keyString + '</div>').insertBefore($(this));
+                        $(this).remove();
                     }
-                    keyString = keyString.trim();
-                    keyString = keyString.slice(-1) == ',' ? keyString.substring(0, keyString.length - 1) : keyString;
-                    $('<div>' + keyString + '</div>').insertBefore($(this));
-                    $(this).remove();
                 }
             });
         }
