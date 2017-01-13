64af6b72fa9095603a9189819ba80cefe32dfe66
David Maser <davidemaser@gmail.com>
2017-01-13 16:53:01 -0500

    Multiple add-ons

--- src/pikl.js
+++ src/pikl.js
@@ -3,7 +3,18 @@
  */
 var Pikl = {
     Config: {
+        defaults:{
+            tplTag:{
+                replacement:'div'
+            }
+        },
         targets:['div','span','p'],
+        reserved:[
+            {'\'':'&apos;'},
+            {'\"':'&quote;'},
+            {'\>':'&gt;'},
+            {'\<':'&lt;'}
+        ],
         brackets:'{}',
         comments:'{..}',
         plugins: {
@@ -42,37 +53,48 @@ var Pikl = {
             })
         },
         Content:function(node){
-           var appendComma = false,targetObject={},targetItem,targetBinding='',targetNode,targetContent,conditional,conditionType,conditionArgument,conditionCase={},t;
+            var appendComma = false, targetObject = {}, targetItem, targetBinding = '', targetNode, targetContent, keyString, layoutObjects, nodeContent, sliceText, objectIndex, calcString, calcMethod,
+                calcValues, cleanObject, conditional, conditionType, conditionArgument, conditionCase = {}, dateString, toRemove, passContent, _this, ajaxString, ajaxParams, ajaxObject, returnedData, param, value,
+                c, d, l, p, t, r;
             node = node || 'body';
             $('tpl').each(function(){
-                var keyString = '';
+                keyString = '';
                 targetBinding = $(this).attr('is');
                 targetObject = pi[targetBinding];
                 targetContent = $(this).html();
                 targetItem = $(this);
                 if(targetBinding == 'layout'){
-                    var layoutObjects = targetContent.split('--');
-                    for(var l in layoutObjects){
+                    layoutObjects = targetContent.split('--');
+                    objectIndex = 0;
+                    for(l in layoutObjects){
+                        var objectLength = layoutObjects.length;
                         if(layoutObjects[l] !== undefined && layoutObjects[l] !== '') {
                             if(layoutObjects[l].indexOf('[')>-1 && layoutObjects[l].indexOf(']')>-1){
-                                var nodeContent = layoutObjects[l].split('[')[1].split(']')[0];
-                                var sliceText = {};
+                                nodeContent = layoutObjects[l].split('[')[1].split(']')[0];
+                                sliceText = {};
                                 sliceText.start = layoutObjects[l].indexOf('[');
                                 sliceText.end = layoutObjects[l].indexOf(']');
                                 sliceText.content = nodeContent;
-                                var toRemove = layoutObjects[l].slice(sliceText.start,sliceText.end+1);
-                                var cleanObject = layoutObjects[l].replace(toRemove,'');
-                                console.log(sliceText.content,cleanObject);
+                                toRemove = layoutObjects[l].slice(sliceText.start,sliceText.end+1);
+                                cleanObject = layoutObjects[l].replace(toRemove,'');
                             }else{
+                                sliceText = {};
                                 cleanObject = layoutObjects[l];
                             }
-                            if(layoutObjects[l].indexOf('*')>-1){
-                                $($p.Assistants.TagBuilder(cleanObject.split('*')[0],cleanObject.split('*')[1]),sliceText.content).insertBefore($(this));
-                                $(this).remove();
+                            if(sliceText.content !== undefined && sliceText.content.indexOf(',') > -1){
+                                passContent = sliceText.content.split(',');
                             }else{
-                                $($p.Assistants.TagBuilder(cleanObject,null,sliceText.content)).insertBefore($(this));
+                                passContent = sliceText.content;
                             }
-
+                            if(layoutObjects[l].indexOf('*')>-1){
+                                $($p.Assistants.TagBuilder(cleanObject.split('*')[0],cleanObject.split('*')[1],passContent)).insertBefore($(this));
+                            }else{
+                                $($p.Assistants.TagBuilder(cleanObject,null,passContent)).insertBefore($(this));
+                            }
+                        }
+                        objectIndex ++;
+                        if(objectIndex == objectLength){
+                            $(this).remove();
                         }
                     }
                 }else if(targetBinding == 'form'){
@@ -93,6 +115,7 @@ var Pikl = {
                         }
                     }
                 }else if($(this).parent().attr('is') !== 'form'){
+                    var tag = $p.Config.defaults.tplTag.replacement;
                     if (targetContent.indexOf('@') > -1) {
                         //we have functions
                         conditional = targetContent.split('@')[1].split('}')[0];
@@ -108,16 +131,16 @@ var Pikl = {
                                 if (conditionArgument.indexOf('and') > -1) {
                                     conditionArgument = conditionArgument.split('and');
                                 }
-                                for (var c in conditionArgument) {
+                                for (c in conditionArgument) {
                                     var calc = conditionArgument[c].trim();
                                     calc = calc.split(' ');
-                                    for (var d in calc) {
+                                    for (d in calc) {
                                         if ($.inArray(calc[d], $p.DateConditions) > -1) {
                                             if ($p.Assistants.Comparison(dateUnit[calc[d]], calc[1], parseInt(calc[2]))) {
-                                                $('<div>' + conditionCase.true + '</div>').insertBefore($(this));
+                                                $('<'+tag+'>' + conditionCase.true + '</'+tag+'>').insertBefore($(this));
                                                 $(this).remove();
                                             } else {
-                                                $('<div>' + conditionCase.false + '</div>').insertBefore($(this));
+                                                $('<'+tag+'>' + conditionCase.false + '</'+tag+'>').insertBefore($(this));
                                                 $(this).remove();
                                             }
                                         }
@@ -125,26 +148,26 @@ var Pikl = {
                                 }
                                 break;
                             case 'calc':
-                                var calcString = targetContent.replace('{@calc}', '').replace('{/calc}', '');
-                                var calcMethod = calcString.split('{@')[1].split('}')[0];
-                                var calcValues = calcString.split('{@' + calcMethod + '}');
-                                $('<div>' + $p.Assistants.Calculate(calcValues[0], calcMethod, calcValues[1]) + '</div>').insertBefore($(this));
+                                calcString = targetContent.replace('{@calc}', '').replace('{/calc}', '');
+                                calcMethod = calcString.split('{@')[1].split('}')[0];
+                                calcValues = calcString.split('{@' + calcMethod + '}');
+                                $('<'+tag+'>' + $p.Assistants.Calculate(calcValues[0], calcMethod, calcValues[1]) + '</'+tag+'>').insertBefore($(this));
                                 $(this).remove();
                                 break;
                             case 'date':
-                                var dateString = targetContent.replace('{@date}', '').replace('{/date}', '');
-                                $('<div>' + $p.Assistants.Date(true) + '</div>').insertBefore($(this));
+                                dateString = targetContent.replace('{@date}', '').replace('{/date}', '');
+                                $('<'+tag+'>' + $p.Assistants.Date(true) + '</'+tag+'>').insertBefore($(this));
                                 $(this).remove();
                                 break;
                             case 'ajax':
-                                var _this = this;
-                                var ajaxString = targetContent.replace('{@ajax}', '').replace('{/ajax}', '');
-                                var ajaxParams = ajaxString.split(',');
-                                var ajaxObject = {};
-                                var returnedData = '';
-                                for (var p in ajaxParams) {
-                                    var param = ajaxParams[p].split('=')[0].replace('}', '').replace('{', '').replace('@', '');
-                                    var value = ajaxParams[p].split('=')[1].replace('}', '').replace('{', '').replace('@', '');
+                                _this = this;
+                                ajaxString = targetContent.replace('{@ajax}', '').replace('{/ajax}', '');
+                                ajaxParams = ajaxString.split(',');
+                                ajaxObject = {};
+                                returnedData = '';
+                                for (p in ajaxParams) {
+                                    param = ajaxParams[p].split('=')[0].replace('}', '').replace('{', '').replace('@', '');
+                                    value = ajaxParams[p].split('=')[1].replace('}', '').replace('{', '').replace('@', '');
                                     if ($.inArray(param, $p.Ajax.params) > -1) {
                                         ajaxObject[param] = value;
                                     }
@@ -152,11 +175,11 @@ var Pikl = {
                                 $p.Assistants.Ajax(ajaxObject).done(function (result) {
                                     result = ajaxObject.index !== undefined && ajaxObject.index !== '' ? result[ajaxObject.index] : result;
                                     if (ajaxObject.repeat == true || ajaxObject.repeat === 'true') {
-                                        for (var r in result) {
+                                        for (r in result) {
                                             returnedData += ajaxObject.node !== undefined && ajaxObject.node !== '' ? result[r][ajaxObject.node] !== undefined ? result[r][ajaxObject.node] + ' ' : '' : result[r] + ' ';
                                         }
                                     }
-                                    $('<div>' + returnedData + '</div>').insertBefore($(_this));
+                                    $('<'+tag+'>' + returnedData + '</'+tag+'>').insertBefore($(_this));
                                     $(_this).remove();
                                 }).fail(function () {
                                     console.log('could not load json data');
@@ -176,7 +199,7 @@ var Pikl = {
                         }
                         keyString = keyString.trim();
                         keyString = keyString.slice(-1) == ',' ? keyString.substring(0, keyString.length - 1) : keyString;
-                        $('<div>' + keyString + '</div>').insertBefore($(this));
+                        $('<'+tag+'>' + keyString + '</'+tag+'>').insertBefore($(this));
                         $(this).remove();
                     }
                 }
@@ -186,7 +209,6 @@ var Pikl = {
     Form:{
         closures:['select','input','button','textarea','form'],
         Build:function(obj,target){
-            console.log(obj,target);
             if(obj !== undefined && typeof obj == 'object') {
                 target = target || $('tpl[is="form"]');
                 var formTemplate = '<form {{attributes}} name="{{name}}" action="{{action}}">{{formItems}}</form>';
@@ -316,6 +338,22 @@ var Pikl = {
                     break;
             }
         },
+        SwapReserved:function(a){
+            var res = $p.Config.reserved;
+            for(var r in res){
+                if(typeof res[r] == 'object'){
+                    var sub = res[r];
+                    for(var s in sub){
+                        if(a !== undefined) {
+                            a = a.replace( new RegExp( s, 'g' ), sub[s] );
+                        }
+                    }
+                }else{
+                    a = a.replace( new RegExp( r, 'g' ), res[r] );
+                }
+            }
+            return a;
+        },
         Calculate: function (a, b, c) {
             switch (b) {
                 case 'times':
@@ -349,20 +387,27 @@ var Pikl = {
             s.i = {tag:'<i>',close:true};
             s.b = {tag:'<strong>',close:true};
             s.nav = {tag:'<nav>',close:true};
-
+            s.section = {tag:'<section>',close:true};
+            s.header = {tag:'<header>',close:true};
+            s.footer = {tag:'<footer>',close:true};
             var tagDisplay = s[tag].tag;
-            if(s[tag].close == true) {
-                var closure = tagDisplay.replace('<','</');
-                tagDisplay += content !== undefined && content !== null ? content : '' ;
-                tagDisplay += closure;
-            }
             if(multiplier !== undefined && multiplier !== null){
                 var tagOutput = '';
                 for(var i = 0;i<multiplier;i++){
                     tagOutput += tagDisplay;
+                    tagOutput += $p.Assistants.SwapReserved(content[i]);
+                    if(s[tag].close == true) {
+                        tagOutput += tagDisplay.replace('<','</');
+                    }
                 }
             }else{
                 tagOutput = tagDisplay;
+                if(s[tag].close == true) {
+                    if(content !== undefined){
+                        tagOutput += $p.Assistants.SwapReserved(content);
+                    }
+                    tagOutput += tagDisplay.replace('<','</');
+                }
             }
             return tagOutput;
         }
